11499f90ff26acf09fc935dc6cebfbe7c96f55bb
Correlo <mamagomi@gmail.com>
2021-08-17 10:23:08 +0100

    Library with my constants

---
+++ const.py
@@ -0,0 +1,29 @@
+from math import pi
+
+MH=1.67339e-27
+ME=9.1094e-31
+MI=1.6726219e-27
+
+
+MEH = 9.1044436e-31
+
+BK=1.380662e-23
+MIH = 8.3669500e-28
+MU0=4.0e-7*pi
+
+CC=2.99792458e8
+EPS0=1.0/(MU0 * CC**2)
+ECHARGE=1.6022e-19
+
+RGAS=8.3143
+mu_n0 = 1e-3
+mu_c0 = 0.5 * mu_n0
+G=2.7398e2
+H_ION_ENERGY =  2.176e-18 # 13.6 eV
+HH_CONST=6.626176e-34
+gamma=1.6666667
+
+SIGMA_IN=1.0e-19
+SIGMA_EN=1.0e-19
+
+
